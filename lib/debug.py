9871bb21e8378d37776cc1d25e44d556603aef05
stella-mumbi <stella.mumbi@student.moringaschool.com>
2024-03-04 11:09:27 +0300

    complted assignment

--- lib/debug.py
+++ lib/debug.py
@@ -1,6 +1,46 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
 from dog import Dog
 import ipdb
-
 ipdb.set_trace()
+
+APPROVED_BREEDS = [
+    "Mastiff",
+    "Chihuahua",
+    "Corgi",
+    "Shar Pei",
+    "Beagle",
+    "French Bulldog",
+    "Pug",
+    "Pointer"
+]
+
+class Dog:
+    def __init__(self, name='Fido', breed='Mastiff'):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            raise ValueError(
+                "Name must be a string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            raise ValueError("Breed must be in the list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
+
+dog_instance = Dog("Fido", "Corgi")
